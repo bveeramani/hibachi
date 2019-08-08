7a9e480022aca9e5ad29e6ca092b8ef467944f31
Balaji Veeramani <bveeramani@berkeley.edu>
2019-08-08 18:36:51 -0500

    Refactor code

--- README.md
+++ README.md
@@ -1,37 +1,12 @@
-# FSPY
-**FSPY** (**F**eature **S**election **Py**thon) is a Python package that
-provides routines for running and comparing various feature selection algorithms.
+# Hibachi
+**Hibachi** is a Python package that implements feature selection methods for
+PyTorch.
 
-## Example
+## Installation
+Run the following command:
+
 ```
-from fspy.algorithms import pearson_rank, pearson_select
-from fspy.datasets import OteyP450
-from fspy.measurements import measure_stability, measure_accuracy, spearman_coefficient, tanimoto_distance
-
-dataset = OteyP450("data/OteyP450/raw/otey_P450_binary_function.txt")
-
-rank_stability = measure_stability(pearson_rank,
-                                   dataset,
-                                   stability_measure=spearman_coefficient,
-                                   num_folds=5)
-print(rank_stability)
-
-selection_stabilities = []
-for num_features in range(1, 25):
-    selection_algorithm = lambda dataset: pearson_select(dataset, num_features)
-    stability = measure_stability(selection_algorithm,
-                                  dataset,
-                                  stability_measure=tanimoto_distance,
-                                  num_folds=2)
-    selection_stabilities.append(stability)
-print(selection_stabilities)
-
-accuracies = []
-for num_features in range(1, 25):
-    feature_subset = pearson_select(dataset, num_features)
-    accuracy = measure_accuracy(feature_subset, dataset, num_folds=5)
-    accuracies.append(accuracy)
-print(accuracies)
+pip install hibachi
 ```
 
 ## References
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,44 +1,40 @@
-"""Tests for fspy.utils"""
+# Copyright 2019 Balaji Veeramani. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for hibachi.utils"""
 import unittest
 
-from torch.utils.data import Dataset
+import torch
 
-from fspy import utils
-
-
-class UtilityFunctionTest(unittest.TestCase):
-
-    def test_remove_unselected_features(self):
-        dataset = DummyDataset()
-        dataset = utils.eliminate(dataset, set([1]))
-
-        self.assertEqual([x for x, y in dataset], [[0], [0], [1], [1]])
-        self.assertEqual([y for x, y in dataset], [0, 1, 0, 1])
-
-    def test_create_folds(self):
-        dataset = DummyDataset()
-        folds = utils.fold(dataset, 3)
-
-        self.assertEqual(len(folds), 3)
-
-        # Three folds of size 1 x 1 x 2
-        for fold_number, (training_fold, testing_fold) in enumerate(folds):
-            if fold_number < 3 - 1:
-                self.assertEqual(len(training_fold), 3)
-                self.assertEqual(len(testing_fold), 1)
-            else:
-                self.assertEqual(len(folds[2][0]), 2)
-                self.assertEqual(len(folds[2][1]), 2)
+from hibachi import utils
 
 
-class DummyDataset(Dataset):
+class UtilsTest(unittest.TestCase):
 
-    def __init__(self):
-        self.xy = [[0, 0], [0, 1], [1, 0], [1, 1]]
-        self.z = [0, 1, 0, 1]  # f(x, y) = y
+    def test_project(self):
+        v = torch.ones(2, dtype=torch.float)
 
-    def __getitem__(self, index):
-        return self.xy[index], self.z[index]
+        actual = utils.project(v, 1)
+        expected = torch.tensor([0.5, 0.5], dtype=torch.float)
 
-    def __len__(self):
-        return 4
+        self.assertTrue(isinstance(actual, torch.FloatTensor))
+        self.assertTrue(torch.equal(actual, expected))
+
+    def test_center(self):
+        X = torch.arange(1, 5).reshape(2, 2)
+
+        actual = utils.center(X)
+        expected = torch.zeros(2, 2, dtype=torch.float)
+
+        self.assertTrue(isinstance(actual, torch.FloatTensor))
+        self.assertTrue(torch.equal(actual, expected))
